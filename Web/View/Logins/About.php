2babb2691d49c2fd75bdf37fe20bb36a05264027
Kgantsi Rabatseta <kgantsi.rabatseta@deposita.co.za>
2016-02-03 07:47:21 +0200

    Added Files

---
+++ Web/View/Logins/About.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+	About US
+</title>
+<link type="text/css" rel="stylesheet" href="../../Styles/MainStyle.css"></link>
+<link type="text/css" rel="stylesheet" href="../../Styles/bootstrap.css"></link>
+<link type="text/css" rel="stylesheet" href="../../Styles/loginTableStyles.css"></link>
+ <script type="text/javascript" src="../../Scripts/TimeScript.js"></script>
+
+</head>
+<body>
+	<!-- Header with Links-->
+		<header>
+			<span class="timeSpan" id="timeKeeper"></span>
+				<script type="text/javascript">window.onload = setTimeS("timeKeeper");</script>
+			<h1 style="top:0%;" class="content">Rabatseta.co</h1><br/>
+			<nav>
+			<a class="menuLinks" href="Home.php">Home</a>
+			<a class="menuLinks" href="About.php">About</a>
+			<a class="menuLinks" href="Contact.php">Contact-Us</a>
+			</nav>
+			
+		</header>
+	<!-- Body Content-->
+		<div  class="bodySection">
+		
+			<h2 clas="header" >
+				About Rabatseta.co.za
+			</h2><br/>
+			<div class="contentShow">
+			<span>
+				This is a works in progress application...<br/>
+				Watch the space for new updates.
+			</span>
+			</div>
+		</div>
+	<!-- Footer With Copyright-->
+		<footer>
+			<p>&copy; 2016  Rabatseta.co <br/>
+    		Date: <time id="dateTimeLocal"></time>
+    		</p>
+    		<script type="text/javascript">window.onload = setDateS("dateTimeLocal");</script>
+		</footer>
+</body>
+</html>
---
+++ Web/View/Logins/Contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+	Contact Us
+</title>
+<link type="text/css" rel="stylesheet" href="../../Styles/MainStyle.css"></link>
+<link type="text/css" rel="stylesheet" href="../../Styles/bootstrap.css"></link>
+<link type="text/css" rel="stylesheet" href="../../Styles/loginTableStyles.css"></link>
+ <script type="text/javascript" src="../../Scripts/TimeScript.js"></script>
+
+</head>
+<body>
+	<!-- Header with Links-->
+		<header>
+			<span class="timeSpan" id="timeKeeper"></span>
+				<script type="text/javascript">window.onload = setTimeS("timeKeeper");</script>
+			<h1 style="top:0%;" class="content">Rabatseta.co</h1><br/>
+			<nav>
+			<a class="menuLinks" href="Home.php">Home</a>
+			<a class="menuLinks" href="About.php">About</a>
+			<a class="menuLinks" href="Contact.php">Contact-Us</a>
+			</nav>
+			
+		</header>
+	<!-- Body Content-->
+		<div class="bodySection">
+		
+			<h2 clas="header" >
+				Contact Rabatseta.co.za
+			</h2><br/>
+		<div class="contentShow">
+			<span>
+				Contact the owners of Rabatseta.co.za
+			</span>
+			<p>
+				E-mail :
+				&nbsp;&nbsp; <ul>
+					<li>
+						<a class="emailLink" href="mailto:kgantsi.rabatseta@gmail.com">
+								kgantsi.rabatseta@gmail.com</a>
+					</li>
+					<li>
+						<a class="emailLink" href=mailto:kgantsi@Rabatseta.co.za">
+							kgantsi@Rabatseta.co.za</a>
+					</li>
+				</ul>	
+			</p>
+			<p>
+				Cell Phone : +2778 427 6308   [Kgantsi Rabatseta]
+			</p>
+			</div>
+		</div>
+	<!-- Footer With Copyright-->
+		<footer>
+			<p>&copy; 2016  Rabatseta.co <br/>
+    		Date: <time id="dateTimeLocal"></time>
+    		</p>
+    		<script type="text/javascript">window.onload = setDateS("dateTimeLocal");</script>
+		</footer>
+</body>
+</html>
